50e57328292691d370b71cbc7b24b01eda3b2d9c
Andy Hayden <andyhayden1@gmail.com>
2019-11-06 17:04:45 -0800

    Initial commit

---
+++ example.ts
@@ -0,0 +1,8 @@
+import { Context, Event } from "https://deno.land/x/lambda/mod.ts";
+
+export async function handler(event: Event, context: Context) {
+  return {
+    statusCode: 200,
+    body: `Welcome to deno ${Deno.version.deno} 🦕`
+  };
+}
