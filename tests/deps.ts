5d3e8250cb45a0c4e09a5ec02d0070bc77fc59ec
Andy Hayden <andyhayden1@gmail.com>
2019-11-14 23:59:33 -0800

    :whale: bump version to 0.24.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,3 +1,3 @@
-export { assertEquals } from "https://deno.land/std@v0.23.0/testing/asserts.ts";
-export { serve } from "https://deno.land/std@v0.23.0/http/server.ts";
-export { runIfMain, test } from "https://deno.land/std@v0.23.0/testing/mod.ts";
+export { assertEquals } from "https://deno.land/std@v0.24.0/testing/asserts.ts";
+export { serve } from "https://deno.land/std@v0.24.0/http/server.ts";
+export { runIfMain, test } from "https://deno.land/std@v0.24.0/testing/mod.ts";
