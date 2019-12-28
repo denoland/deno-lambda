672e4eac402f4b92e5251a9ebb69fa04abe17879
Andy Hayden <andyhayden1@gmail.com>
2019-12-28 11:42:27 +0000

    Use aws exported types in the tests

--- hello.ts
+++ hello.ts
@@ -1,6 +1,6 @@
-import { Context, Event } from "https://deno.land/x/lambda/mod.ts";
+import { Context, APIGatewayProxyEvent, APIGatewayProxyResult } from "https://deno.land/x/lambda/mod.ts";
 
-export async function handler(event: Event, context: Context) {
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
     body: `Welcome to deno ${Deno.version.deno} 🦕`
--- tests/pad.ts
+++ tests/pad.ts
@@ -1,9 +1,8 @@
-import { Context, Event } from "https://deno.land/x/lambda/mod.ts";
 // pad.ts is chosen since it has no dependencies.
 // i.e. we must download precisely one file from deno.land.
 import { pad } from "https://deno.land/std/strings/pad.ts";
 
-export function handler(event: Event, context: Context) {
+export function handler(event, context) {
   const strLen: number = Number(event.strLen) || 5;
   return pad("deno", strLen);
 }
