b3e7e1a03fbf8136254c8038847be677f22f7539
Andy Hayden <andyhayden1@gmail.com>
2020-01-05 11:23:32 +0000

    Bump version to 0.28.1

--- example/deps.ts
+++ example/deps.ts
@@ -1,11 +1,7 @@
-// FIXME there are no releases of dynamodb atm.
-// export { createClient, Doc, DynamoDBClient } from "https://deno.land/x/dynamodb/mod.ts";
-// We require a patched version of dynamodb to support temporary session tokens:
-// https://github.com/chiefbiiko/dynamodb/pull/3
 export {
   createClient,
   Doc,
   DynamoDBClient
-} from "https://raw.githubusercontent.com/hayd/dynamodb/782f64ce64e7c83f964970c7eeacba0d5c9dfb91/mod.ts";
+} from "https://deno.land/x/dynamodb@v0.2.0/mod.ts";
 
-export { v4 as uuid } from "https://deno.land/std@v0.27.0/uuid/mod.ts";
+export { v4 as uuid } from "https://deno.land/std@v0.28.1/uuid/mod.ts";
