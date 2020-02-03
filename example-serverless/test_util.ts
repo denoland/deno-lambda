7121da1ac1c9bdd793851fb33d93b29b57cb644f
Andy Hayden <andyhayden1@gmail.com>
2020-02-03 14:21:36 -0800

    Bump version to 0.32.0

--- example-serverless/test_util.ts
+++ example-serverless/test_util.ts
@@ -1,7 +1,7 @@
 import {
   TestDefinition,
   test as stdTest
-} from "https://deno.land/std@v0.31.0/testing/mod.ts";
+} from "https://deno.land/std@v0.32.0/testing/mod.ts";
 
 import { client } from "./client.ts";
 import { TableName } from "./api/candidate.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,6 +1,6 @@
-export { assertEquals } from "https://deno.land/std@v0.31.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@v0.32.0/testing/asserts.ts";
 export {
   serve,
   ServerRequestBody
-} from "https://deno.land/std@v0.31.0/http/server.ts";
-export { runIfMain, test } from "https://deno.land/std@v0.31.0/testing/mod.ts";
+} from "https://deno.land/std@v0.32.0/http/server.ts";
+export { runIfMain, test } from "https://deno.land/std@v0.32.0/testing/mod.ts";
