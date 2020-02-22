7a4392f809f2b11d76461e9a57bdba9273d464f2
Andy Hayden <andyhayden1@gmail.com>
2020-02-21 23:20:50 -0800

    Use strict mode in all files
    Actually set the deno version to 0.34.0

--- tests/decorate.ts
+++ tests/decorate.ts
@@ -1,10 +1,15 @@
-function decorate(target) {}
+import {
+  APIGatewayProxyEvent,
+  Context
+} from "https://deno.land/x/lambda/mod.ts";
+
+function decorate(target: any) {}
 
 @decorate
 class Foo {
 }
 
-export async function handler(event, context) {
+export async function handler(event: APIGatewayProxyEvent, context: Context) {
   return {
     statusCode: 200,
     body: `decorated 🦕`
--- tests/importmap.ts
+++ tests/importmap.ts
@@ -1,6 +1,10 @@
+import {
+  APIGatewayProxyEvent,
+  Context
+} from "https://deno.land/x/lambda/mod.ts";
 import { pad } from "std/strings/pad.ts";
 
-export function handler(event, context) {
+export function handler(event: any, context: Context) {
   const strLen: number = Number(event.strLen) || 5;
   return pad("deno", strLen);
 }
