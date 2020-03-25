1371d70d0d7be3e1329c870d9429f78e0d5f2d7b
Andy Hayden <andyhayden1@gmail.com>
2020-03-24 21:49:06 -0700

    Update serverless-example dependencies and test in CI
    As an additional job (rather than as another .yml file)

--- example-serverless/client.ts
+++ example-serverless/client.ts
@@ -1,11 +1,3 @@
 import { DynamoDBClient, createClient } from "./deps.ts";
 
-const conf = {
-  accessKeyId: Deno.env("AWS_ACCESS_KEY_ID"),
-  secretAccessKey: Deno.env("AWS_SECRET_ACCESS_KEY"),
-  sessionToken: () => Deno.env("AWS_SESSION_TOKEN"),
-  region: Deno.env("AWS_REGION") || "local",
-  port: Deno.env("AWS_REGION") ? 443 : 8000
-};
-
-export const client: DynamoDBClient = createClient(conf);
+export const client: DynamoDBClient = createClient();
