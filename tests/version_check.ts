fc0f8a728cc02fd6472cecea715896fc8b836a02
Andy Hayden <andyhayden1@gmail.com>
2020-03-29 14:12:23 -0700

    Require passing DENO_LAMBDA_VERSION to run version_check

--- tests/version_check.ts
+++ tests/version_check.ts
@@ -2,7 +2,5 @@ import { assertEquals } from "./deps.ts";
 
 Deno.test(function versionCheck() {
   const v = Deno.env("DENO_LAMBDA_VERSION");
-  if (v !== undefined) {
-    assertEquals(v, Deno.version.deno);
-  }
+  assertEquals(v, Deno.version.deno);
 });
