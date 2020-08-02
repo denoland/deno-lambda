64bde0da2f174cbb1b032bdcdd743bc7a729296b
Luca Casonato <lucacasonato@yahoo.com>
2020-08-02 23:10:00 +0200

    Bump to Deno 1.2.2

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,5 +1,5 @@
 export {
   assert,
   assertEquals
-} from "https://deno.land/std@0.62.0/testing/asserts.ts";
-export { serve } from "https://deno.land/std@0.62.0/http/server.ts";
+} from "https://deno.land/std@0.63.0/testing/asserts.ts";
+export { serve } from "https://deno.land/std@0.63.0/http/server.ts";
