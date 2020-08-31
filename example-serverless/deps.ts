657705d74a1351bb93940a99a4c2b7c7e80ab53c
Luca Casonato <lucacasonato@yahoo.com>
2020-08-31 18:03:19 +0200

    Bump Deno to 1.3.2

--- example-serverless/deps.ts
+++ example-serverless/deps.ts
@@ -1,8 +1,8 @@
 export {
   createClient,
   Doc,
-  DynamoDBClient
+  DynamoDBClient,
 } from "https://deno.land/x/dynamodb@v1.0.0/mod.ts";
 
-import { v4 } from "https://deno.land/std@0.66.0/uuid/mod.ts";
+import { v4 } from "https://deno.land/std@0.67.0/uuid/mod.ts";
 export const uuid = v4.generate;
--- runtime/generate_types.ts
+++ runtime/generate_types.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.66.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.67.0/testing/asserts.ts";
 
 const unpkg = "https://unpkg.com/@types/aws-lambda@8.10.59/";
 
