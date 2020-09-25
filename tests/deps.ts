332fe739bf40fdb117bdb377376f9b3b05479ebd
William Perron <thewilliamperron@gmail.com>
2020-09-25 17:11:35 -0400

    Bump to Deno 1.4.2

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,5 +1,5 @@
 export {
   assert,
   assertEquals
-} from "https://deno.land/std@0.70.0/testing/asserts.ts";
-export { serve } from "https://deno.land/std@0.70.0/http/server.ts";
+} from "https://deno.land/std@0.71.0/testing/asserts.ts";
+export { serve } from "https://deno.land/std@0.71.0/http/server.ts";
