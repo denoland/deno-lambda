6a61016f29dc3782d9c263ac718484798f58f17e
Andy Hayden <andyhayden1@gmail.com>
2021-11-09 21:35:55 +0000

    Update tests to std 0.114.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,6 +1,6 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.93.0/testing/asserts.ts";
-export { readAll } from "https://deno.land/std@0.93.0/io/util.ts";
-export { serve } from "https://deno.land/std@0.93.0/http/server.ts";
+} from "https://deno.land/std@0.114.0/testing/asserts.ts";
+export { readAll } from "https://deno.land/std@0.114.0/io/util.ts";
+export { serve } from "https://deno.land/std@0.114.0/http/server.ts";
