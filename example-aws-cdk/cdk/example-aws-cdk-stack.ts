7df33a1c9740cc473d552388479fd7dc6c1e08de
GitHub <noreply@github.com>
2022-01-20 17:44:21 +0000

    Bump deno version to 1.18.0

--- example-aws-cdk/cdk/example-aws-cdk-stack.ts
+++ example-aws-cdk/cdk/example-aws-cdk-stack.ts
@@ -11,7 +11,7 @@ export class ExampleAwsCdkStack extends cdk.Stack {
       location: {
         applicationId:
           "arn:aws:serverlessrepo:us-east-1:390065572566:applications/deno",
-        semanticVersion: "1.17.3",
+        semanticVersion: "1.18.0",
       },
     });
 
